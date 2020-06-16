1409b23bf7a99329c4709e57995e586f2ddc642b
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-16 12:43:06 +0300

    added posting a post to the database

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -1,4 +1,5 @@
 const { check, validationResult } = require("express-validator");
+const Post = require("../models/post");
 
 const route = require("express").Router();
 
@@ -24,21 +25,27 @@ route.post(
   "/post",
   check("title").trim().isLength({ min: 5 }),
   check("content").trim().isLength({ min: 5 }),
-  (req, res) => {
-    const { content, title } = req.body;
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).send({ message: "Error, Validation failed" });
-    }
-    res.status(201).send({
-      post: {
-        _id: new Date().toISOString(),
+  async (req, res) => {
+    try {
+      const { content, title } = req.body;
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(422).send({ message: "Error, Validation failed" });
+      }
+      const post = new Post({
         content,
         title,
         creator: { name: "kevin" },
-        createdAt: new Date()
-      }
-    });
+        imageUrl:
+          "https://images.unsplash.com/photo-1484417894907-623942c8ee29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=889&q=80"
+      });
+      const savedPost = await post.save();
+      res.status(201).send({
+        post: savedPost
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 );
 
