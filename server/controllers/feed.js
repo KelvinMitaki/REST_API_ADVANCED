d8d8091e67ddee85ac06109e7138c268773539df
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-16 15:16:43 +0300

    added logic to update a post

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -1,7 +1,10 @@
-const { check, validationResult } = require("express-validator");
-const Post = require("../models/post");
+const fs = require("fs");
+const path = require("path");
 
 const route = require("express").Router();
+const { check, validationResult } = require("express-validator");
+
+const Post = require("../models/post");
 
 route.get("/posts", async (req, res) => {
   try {
@@ -45,10 +48,55 @@ route.post(
 route.get("/post/:postId", async (req, res) => {
   try {
     const post = await Post.findById(req.params.postId);
+    if (!post) {
+      return res.status(404).send({ message: "No post found" });
+    }
     res.send(post);
   } catch (error) {
     res.status(500).send(error);
   }
 });
+
+route.put(
+  "/post/:postId",
+  check("title").trim().isLength({ min: 5 }),
+  check("content").trim().isLength({ min: 5 }),
+  async (req, res) => {
+    try {
+      const postId = req.params.postId;
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(422).send({ message: "Validation failed" });
+      }
+      const { content, title } = req.body;
+      let imageUrl = req.body.image;
+      if (req.file) {
+        imageUrl = req.file.path;
+      }
+      if (!imageUrl) {
+        return res.status(422).send({ message: "No file picked" });
+      }
+      const post = await Post.findById(postId);
+      if (!post) {
+        return res.status(404).send({ message: "No post found" });
+      }
+      if (imageUrl !== post.imageUrl) {
+        clearFilePath(post.imageUrl);
+      }
+      post.title = title;
+      post.content = content;
+      post.imageUrl = imageUrl;
+      const updatedPost = await post.save();
+      res.send(updatedPost);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  }
+);
+
+const clearFilePath = filepath => {
+  filepath = path.join(__dirname, "..", filepath);
+  fs.unlink(filepath, err => err && console.log(err));
+};
 
 module.exports = route;
