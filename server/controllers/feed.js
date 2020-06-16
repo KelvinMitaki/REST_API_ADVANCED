d22bc1385c70c22a4167451a9b8745a408a9d788
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-16 10:15:58 +0300

    added a requests to fetch a list of posts

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -1,3 +1,21 @@
 const route = require("express").Router();
 
+route.get("/posts", (req, res) => {
+  res.send({
+    posts: [
+      {
+        _id: new Date().toISOString(),
+        title: "first post",
+        content: "this is the first post",
+        imageUrl:
+          "https://images.unsplash.com/photo-1484417894907-623942c8ee29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=889&q=80",
+        creator: {
+          name: "kevin"
+        },
+        createdAt: new Date()
+      }
+    ]
+  });
+});
+
 module.exports = route;
